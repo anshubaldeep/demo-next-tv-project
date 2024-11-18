7c3470873f6748be346b81ef8e1c98099378871f
Baldeep Singh <anshubaldeep@gmail.com>
2024-11-19 00:25:22 +0530

    Tried using react tv space navigation but react native neede

---
+++ src/components/remote/configureRemoteControl.ts
@@ -0,0 +1,29 @@
+import { Directions, SpatialNavigation } from 'react-tv-space-navigation';
+
+import RemoteControlManager from './remote-control/RemoteControlManager';
+import { SupportedKeys } from './remote-control/SupportedKeys';
+
+SpatialNavigation.configureRemoteControl({
+  remoteControlSubscriber: (callback) => {
+    const mapping: { [key in SupportedKeys]: Directions | null } = {
+      [SupportedKeys.Right]: Directions.RIGHT,
+      [SupportedKeys.Left]: Directions.LEFT,
+      [SupportedKeys.Up]: Directions.UP,
+      [SupportedKeys.Down]: Directions.DOWN,
+      [SupportedKeys.Enter]: Directions.ENTER,
+      [SupportedKeys.LongEnter]: Directions.LONG_ENTER,
+      [SupportedKeys.Back]: null,
+    };
+
+    const remoteControlListener = (keyEvent: SupportedKeys) => {
+      callback(mapping[keyEvent]);
+      return false;
+    };
+
+    return RemoteControlManager.addKeydownListener(remoteControlListener);
+  },
+
+  remoteControlUnsubscriber: (remoteControlListener) => {
+    RemoteControlManager.removeKeydownListener(remoteControlListener);
+  },
+});
